4664c9abce614fa8f199cac1f2bec1a3c4da2819
Allen Shaw <allen@JoineryHQ.com>
2025-08-24 15:25:39 -0500

    index.php: rough add osrthorizon positions content for display.

--- index.php
+++ index.php
@@ -26,6 +26,8 @@ $wa  = $this->getWebAssetManager();  // Get the Web Asset Manager - used to load
 // Add Favicon from images folder
 $this->addHeadLink(HTMLHelper::_('image', 'favicon.ico', '', [], true, 1), 'icon', 'rel', ['type' => 'image/x-icon']);
 
+// Getting params from template
+$params = $app->getTemplate(true)->params;
 
 // Detecting Active Variables
 $option   = $app->input->getCmd('option', '');
@@ -37,6 +39,7 @@ $sitename = htmlspecialchars($app->get('sitename'), ENT_QUOTES, 'UTF-8');
 $menu     = $app->getMenu()->getActive();
 $pageclass = $menu !== null ? $menu->getParams()->get('pageclass_sfx', '') : '';
 
+/* ******** joomstarter: testparam START
 //Get params from template styling
 //If you want to add your own parameters you may do so in templateDetails.xml
 $testparam =  $this->params->get('testparam');
@@ -46,6 +49,7 @@ $testparam =  $this->params->get('testparam');
 
 // Get this template's path
 $templatePath = 'templates/' . $this->template;
+******** joomstarter: testparam END */
 
 
 //load bootstrap collapse js (required for mobile menu to work)
@@ -63,6 +67,45 @@ $wa->useStyle('template.osrthorizon.template.css');
 $wa->useStyle('template.osrthorizon.user.css');
 $wa->useScript('template.osrthorizon.template.js');
 
+/* ******** fixme: osrthorizon: additional js and css files START
+JHtml::_('script', 'scrollspy.min.js', array('version' => 'auto', 'relative' => true));
+// Include our own copy of swiper.js for ad banner sliders
+JHtml::_('script', 'swiperswiper-bundle.min.js', array('version' => 'auto', 'relative' => true));
+JHtml::_('script', 'custom.js', array('version' => 'auto', 'relative' => true));
+JHtml::_('stylesheet', 'style.css', array('version' => 'auto', 'relative' => true));
+******** fixme: osrthorizon: additional js and css files END */
+
+/* ******** fixme: osrthorizon: adjust witdh based on position content START
+// Adjusting content width
+$position7ModuleCount = $this->countModules('position-7');
+$position8ModuleCount = $this->countModules('position-8');
+$position9ModuleCount = $this->countModules('position-9');
+$position11ModuleCount = $this->countModules('position-11');
+
+if ($position7ModuleCount && $position8ModuleCount)
+{
+	$span = 'span6';
+}
+elseif ($position7ModuleCount && !$position8ModuleCount)
+{
+	$span = 'span9';
+}
+elseif (!$position7ModuleCount && $position8ModuleCount)
+{
+	$span = 'span9';
+}
+else
+{
+	$span = 'span12';
+}
+******** fixme: osrthorizon: adjust witdh based on position content END */
+
+/* ******** fixme: osrthorizon: logo file START
+// Logo file or site title param
+$logo = '<img src="' . htmlspecialchars(JUri::root() . 'templates/osrthorizon/images/logo-header.png', ENT_QUOTES) . '" alt="' . $sitename . '" />';
+******** fixme: osrthorizon: logo file END */
+
+
 //Set viewport meta tag for mobile responsiveness -- very important for scaling on mobile devices
 $this->setMetaData('viewport', 'width=device-width, initial-scale=1');
 
@@ -72,6 +115,9 @@ $this->setMetaData('viewport', 'width=device-width, initial-scale=1');
 <!DOCTYPE html>
 <html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
+<!-- Google tag (gtag.js) -->
+<script async src=https://www.googletagmanager.com/gtag/js?id=G-JBQHYMD224></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-JBQHYMD224');
+ </script> 
 
     <?php // Loads important metadata like the page title and viewport scaling ?>
 	<jdoc:include type="metas" />
@@ -80,7 +126,8 @@ $this->setMetaData('viewport', 'width=device-width, initial-scale=1');
 	<jdoc:include type="styles" />
 	<jdoc:include type="scripts" />
 
-    <?php /** You can put links to CSS/JS just like any regular HTML page here too, and remove the jdoc:include script/style lines above if you want.
+    <?php /** joomstarter comment: 
+     * You can put links to CSS/JS just like any regular HTML page here too, and remove the jdoc:include script/style lines above if you want.
      * Do not delete the metas line though
      * 
      * For example, if you want to manually link to a custom stylesheet or script, you can do it like this:
@@ -92,8 +139,34 @@ $this->setMetaData('viewport', 'width=device-width, initial-scale=1');
 </head>
 
 <?php // you can change data-bs-theme to dark for dark mode  // ?>
-<body class="site <?php echo $pageclass; ?>" data-bs-theme="light">
-	<header>
+<body class="site <?php echo $pageclass; ?> <?php echo $option
+	. ' view-' . $view
+	. ($layout ? ' layout-' . $layout : ' no-layout')
+	. ($task ? ' task-' . $task : ' no-task')
+	. ($itemid ? ' itemid-' . $itemid : ' no-item')
+	// . ($params->get('fluidContainer') ? ' fluid' : '')
+  ;
+?>" data-bs-theme="light">
+  <header class="header">
+    <div class="header__left">
+				<div class="header-inner clearfix">
+					<a class="brand pull-left" href="<?php echo $this->baseurl; ?>/">
+						<?php echo $logo; ?>
+						<?php if ($this->params->get('sitedescription')) : ?>
+							<?php echo '<div class="site-description">' . htmlspecialchars($this->params->get('sitedescription'), ENT_COMPAT, 'UTF-8') . '</div>'; ?>
+						<?php endif; ?>
+					</a>
+				</div>
+    </div>      
+    <div class="header__right-bottom">
+        <div class="header-search pull-right">
+            <!-- position-0 START --><jdoc:include type="modules" name="position-0" style="none" /><!-- position-0 END -->
+        </div>
+        <img class="header-img" src="/templates/osrthorizon/images/header-line.png" alt="">
+    </div>      
+    
+    <?php
+    /* ******************** joomstarter: top-bar START
         <?php // Generate a Bootstrap Navbar for the top of our website and put the site title on it ?>
         <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
             <div class="container">
@@ -115,51 +188,65 @@ $this->setMetaData('viewport', 'width=device-width, initial-scale=1');
                 <jdoc:include type="modules" name="header" style="none" />
             </div>
         <?php endif; ?>
-    </header>
-
+    ******************** joomstarter: top-bar END */ ?>
+  </header>
+  <?php if ($this->countModules('position-1')) : ?>
+    <nav class="navigation" role="navigation">
+        <div class="navigation-block">
+            <!-- position-1 START --><jdoc:include type="modules" name="position-1" style="none" /><!-- position-1 END -->
+            <?php if ($position11ModuleCount) : ?>
+              <div class="menu-optional-wrapper">
+                <div class="menu-optional">
+                    <!-- position-11 START --><jdoc:include type="modules" name="position-11" style="none" /><!-- position-11 END -->
+                </div>
+              </div>
+            <?php endif; ?>
+        </div>
+    </nav>
+  <?php endif; ?>
+  <!-- position-3 START --><jdoc:include type="modules" name="position-3" style="none" /><!-- position-3 END -->
+  <!-- position-4 START --><jdoc:include type="modules" name="position-4" style="xhtml" /><!-- position-4 END -->
+  <!-- position-5 START --><jdoc:include type="modules" name="position-5" style="xhtml" /><!-- position-5 END -->
+  <!-- position-7 START --><jdoc:include type="modules" name="position-7" style="well" /><!-- position-7 END -->
+  
     <?php // Generate the main content area of the website ?>
     <div class="siteBody">
         <div class="container">
-        <?php // Load Breadcrumbs Module if Module Exists ?>
-            <?php if ($this->countModules('breadcrumbs')) : ?>
-                <div class="breadcrumbs">
-                    <jdoc:include type="modules" name="breadcrumbs" style="none" />
-                </div>
-            <?php endif; ?>
             <div class="row">
-            <?php // Use a BootStrap grid to load main content on left, sidebar on right IF sidebar exists ?>
-                <?php if ($this->countModules('sidebar')) : ?>
-                <div class="col-xs-12 col-lg-8">
-
-                    <main>
-                    <?php // Load important Joomla system messages ?>
-                        <jdoc:include type="message" />
-                        <?php // Load the main component of the webpage ?>
-                        <jdoc:include type="component" />
-                    </main>
-                </div>
-                <?php // Load the sidebar if one exists ?>
-                <div class="col-xs-12 col-lg-4">
-                <?php // This line tells Joomla to load the "sidebar" module position with the "superBasicMod" mod chrome as the default (see html/layouts/chromes folder) ?>
-                        <jdoc:include type="modules" name="sidebar" style="superBasicMod" />
-                </div>
-                <?php // If there's no sidebar, just load the component with no sidebar ?>
-                <?php else: ?>
-                    <?php // Load the main component of the webpage ?>
-                    <main>
-                        <jdoc:include type="component" />
-                    </main>
-                <?php endif; ?>
+              <main>
+                <!-- message START -->
+                  <jdoc:include type="message" />
+                <!-- message END -->
+                <!-- component START -->
+                  <jdoc:include type="component" />
+                <!-- component END -->
+              </main>
             </div>
         </div>
     </div>
+  
+  
+    <div class="clearfix"></div>
+    <!-- position-9 START --><jdoc:include type="modules" name="position-9" style="none" /><!-- position-9 END -->
+    <div class="clearfix"></div>
 
     <?php // Load Footer ?>
     <footer class="footer mt-auto py-3 bg-light ">
-        <div class="container">
-            <?php if ($this->countModules('footer')) : ?>
+        <div class="wrapper">
+            <div class="footer-block">
+                <img src="/templates/osrthorizon/images/footer-line.png" alt="">
+            </div>
+            <div class="footer-main">
+                <?php echo $logo; ?>
+              <!-- footer: START -->
                 <jdoc:include type="modules" name="footer" style="none" />
-            <?php endif; ?>
+              <!-- footer: END -->
+            </div>
+            <div class="footer-bottom">
+              <!-- footer-2: START -->
+			    <jdoc:include type="modules" name="footer-2" style="none" />
+              <!-- footer-2: END -->
+            </div>
         </div>
     </footer>
 
